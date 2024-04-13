94026dc099a77f31e42d936b6e6a74f280e898db
mass <massaw6@gmail.com>
2024-04-13 17:57:45 +0000

    debug: on svg

--- app/virtual.js
+++ app/virtual.js
@@ -5,6 +5,7 @@ export const virtualObj = {
   },
   children: [
     { tag: "h1", attrs: { class: "title" }, children: ["TODOS"] },
+    //?-------------------- display input zone ---------------------
     {
       tag: "div",
       attrs: {
@@ -24,6 +25,7 @@ export const virtualObj = {
           attrs: {
             class: "buttons",
           },
+          //*--------- buttons -----------------
           children: [
             {
               tag: "button",
@@ -42,7 +44,7 @@ export const virtualObj = {
                     {
                       tag: "path",
                       attrs: {
-                        opacity: "0.8",
+                        opacity: 0.8,
                         d: "M20.7727 20.9717L35.8176 39.4032L59.7954 5.67743",
                         stroke: "white",
                         "stroke-width": "12",
@@ -58,82 +60,133 @@ export const virtualObj = {
                       },
                     },
                   ],
-                },
+                }
               ],
             },
             {
               tag: "button",
               attrs: { id: "add-todo", class: "cmd hover-btn" },
-            },
-          ],
-        },
-        //display tasks
-        { tag: "div", attrs: { class: "middle" } },
-        //footer side
-        {
-          tag: "div",
-          attrs: {
-            id: "footer",
-          },
-          children: [
-            {
-              tag: "p",
-              attrs: { id: "remaining" },
               children: [
                 {
-                  tag: "span",
-                  attrs: { id: "remaining-count" },
-                  children: ["0"],
+                  tag: "svg",
+                  attrs: {
+                    width: "43",
+                    height: "35",
+                    viewBox: "0 0 43 48",
+                    fill: "none",
+                    xmlns: "http://www.w3.org/2000/svg",
+                  },
+                  children: [
+                    {
+                      tag: "rect",
+                      attrs: {
+                        width: "7.30115",
+                        height: "46.9541",
+                        rx: "3.65058",
+                        transform:
+                          "matrix(0.999391 0.0348995 0.00855318 -1.00002 17.9391 47.308)",
+                        fill: "white",
+                        xmlns: "http://www.w3.org/2000/svg",
+                      },
+                    },
+                    {
+                      tag: "rect",
+                      attrs: {
+                        width: "8.29571",
+                        height: "41.3323",
+                        rx: "4.14785",
+                        transform:
+                          "matrix(-0.0174524 -0.999848 -0.99996 -0.0152587 42.1935 28.8464)",
+                        fill: "white",
+                        xmlns: "http://www.w3.org/2000/svg",
+                      },
+                    },
+                  ],
                 },
-                "items left",
               ],
             },
+          ],
+          //*--------- end of buttons -----------------
+        },
+      ],
+    },
+    //?-------------------- end of display input zone ---------------------
+    //********************************************************************/
+    //?-------------------- display tasks ---------------------
+    { tag: "div", attrs: { class: "middle" } },
+    //?------------------- footer side ------------------------
+    {
+      tag: "div",
+      attrs: {
+        id: "footer",
+      },
+      children: [
+        {
+          tag: "p",
+          attrs: { id: "remaining" },
+          children: [
             {
               tag: "span",
-              attrs: { id: "display all footer" },
+              attrs: { id: "remaining-count" },
+              children: ["0"],
+            },
+            "items left",
+          ],
+        },
+        {
+          tag: "div",
+          attrs: { id: "events" },
+          children: [
+            {
+              tag: "span",
+              attrs: { class: "display-all footer  hover-link" },
               children: ["All"],
             },
             {
               tag: "span",
-              attrs: { id: "display active footer" },
+              attrs: { class: "display-active footer hover-link" },
               children: ["Active"],
             },
             {
               tag: "span",
-              attrs: { id: "display done footer" },
+              attrs: { class: "display-done footer hover-link" },
               children: ["Done"],
-            },
-            {
-              tag: "span",
-              attrs: { id: "clear footer" },
-              children: ["Clear Done"],
             },
           ],
         },
-        //credits
         {
-          tag: "div",
-          attrs: { class: "credits" },
+          tag: "span",
+          attrs: { class: "hover-link", id: "clear-footer" },
+          children: ["Clear Completed"],
+        },
+      ],
+    },
+    //?------------------- end of footer side ------------------------
+    //********************************************************************/
+    //?------------ credits ---------------
+    {
+      tag: "div",
+      attrs: { class: "credits" },
+      children: [
+        {
+          tag: "ul",
+          attrs: { id: "credit-list" },
           children: [
             {
-              tag: "ul",
-              attrs: { id: "credit-list" },
-              children: [
-                {
-                  tag: "li",
-                  attrs: { class: "instruction" },
-                  children: ["Double click to edit"],
-                },
-                {
-                  tag: "li",
-                  attrs: { class: "author" },
-                  children: ["Created by @mv"],
-                },
-              ],
+              tag: "li",
+              attrs: { class: "instruction" },
+              children: ["Double click to edit"],
+            },
+            {
+              tag: "li",
+              attrs: { class: "author" },
+              children: ["Created by @mv"],
             },
           ],
         },
       ],
     },
+    //?------------ end of credits ---------------
+    //********************************************************************/
   ],
 };
