08040aa674b057a7c620cdb2ceafc32c145ed5c7
mass <massaw6@gmail.com>
2024-04-17 00:49:31 +0000

    update: footer et toggle all button display on process

---
+++ app/utils/displayUI.mjs
@@ -0,0 +1,4 @@
+const displayUI = () => {
+    const todolistContainer = document.getElementsByClassName("todo-list")[0]
+    if (!todolistContainer) console.error(" todo list container has not been created");
+}
