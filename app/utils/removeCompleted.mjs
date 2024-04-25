73f29c5e32e2ab8d5fe03509525bc95e94331b17
vindour <vincentfelixnd@hotmail.fr>
2024-04-25 14:58:14 +0000

    remove-updated

--- app/utils/removeCompleted.mjs
+++ app/utils/removeCompleted.mjs
@@ -1,10 +1,11 @@
 import { routes } from "../main.mjs";
 import { removeTask } from "../lib/removeTask.mjs";
+import { database } from "../database/database.mjs";
 
-export function removeCompleted(e) {
-  document.querySelectorAll("li").forEach((el) => {
-    if (el.classList.contains("completed"))
-      removeTask(el.children[0].children[2].id);
+export function removeCompleted(e) {  
+  database.set((x) => {
+    x.value = x.value.filter((x) => x.state !== "completed")
+    return x
   });
   e.target.disabled = true;
   routes.loadCurrentView();
